84ae7a6b02ae0c830d75c966baf225636665faf0
Adrianna Nowak <adriannanowakk@gmail.com>
2023-05-16 19:15:28 +0200

    save project before many changes

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,23 @@
+// import { templates, select } from './settings';
+// import { utils } from '../utils.js';
+
+// import { select } from '../settings.js';
 const app = {
   init: function () {
-  },
+
+    fetch('http://localhost:3131/products')
+      .then(response => {
+        console.log('response', response);
+        return response.json();
+      }).then(responseJson => console.log('response json?', responseJson));
+  }
+  // render: function () {
+  //   const thisProduct = this;
+  //   const generatedHTML = templates.allProducts();
+  //   thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+  //   const productsContainer = document.querySelector(select.containerOf.product);
+  //   productsContainer.appendChild(thisProduct.element);
+  // },
 };
 
 app.init();
--- src/js/settings.js
+++ src/js/settings.js
@@ -1,1 +1,18 @@
 // tu bedziemy przechowywac selektory, nazwy klas, ustawienia (np link do serwera API)
+
+export const select = {
+  templateOf: {
+    homePage: '#template-home-page',
+    product: '#template-products',
+    contactPage: '#template-contact-form'
+  },
+  containerOf: {
+    home: '#home',
+    products: '#products',
+    contact: '#contact',
+  }
+};
+
+export const templates = {
+  allProducts: Handlebars.compile(document.querySelector(select.templateOf.product).innerHTML),
+};
