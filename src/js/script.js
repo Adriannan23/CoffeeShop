0549bfa0d543c8d7e25ff15e41a649231c56fb54
Adrianna Nowak <adriannanowakk@gmail.com>
2023-05-18 15:49:39 +0200

    add connectWithDataBase function

--- src/js/script.js
+++ src/js/script.js
@@ -6,54 +6,78 @@
 // app.init();
 
 
-// const data = {
-//   'products': [
-//     {
-//       'id': 1,
-//       'description': 'Opis produktu 1',
-//       'roasting': 'Stopień palenia 1',
-//       'intensity': 'Intensywność 1',
-//       'image': 'coffee-3'
-//     },
-//     {
-//       'id': 2,
-//       'description': 'Opis produktu 2',
-//       'roasting': 'Stopień palenia 2',
-//       'intensity': 'Intensywność 2',
-//       'image': 'coffee-3'
-//     },
-//   ]
-// };
+const data = {
+  'products': [
+    {
+      'id': 1,
+      'description': 'Opis produktu 1',
+      'roasting': 'Stopień palenia 1',
+      'intensity': 'Intensywność 1',
+      'image': 'coffee-3'
+    },
+    {
+      'id': 2,
+      'description': 'Opis produktu 2',
+      'roasting': 'Stopień palenia 2',
+      'intensity': 'Intensywność 2',
+      'image': 'coffee-3'
+    },
+  ]
+};
 
-// document.addEventListener('DOMContentLoaded', function () {
-//   const productsTemplate = document.getElementById('products-template').innerHTML;
-//   console.log('productsTemplate:', productsTemplate);
+document.addEventListener('DOMContentLoaded', function () {
+  const productsTemplate = document.getElementById('products-template').innerHTML;
+  console.log('productsTemplate:', productsTemplate);
 
-//   const compiledProductsTemplate = Handlebars.compile(productsTemplate);
+  const compiledProductsTemplate = Handlebars.compile(productsTemplate);
 
-//   const generatedHtmlForProduct = compiledProductsTemplate({
-//     'id': 1,
-//     'description': 'Opis produktu 1',
-//     'roasting': 'Stopień palenia 1',
-//     'intensity': 'Intensywność 1',
-//     'image': 'coffee-3'
-//   });
+  const generatedHtmlForProduct = compiledProductsTemplate({
+    'id': 1,
+    'description': 'Opis produktu 1',
+    'roasting': 'Stopień palenia 1',
+    'intensity': 'Intensywność 1',
+    'image': 'coffee-3'
+  });
 
-// console.log(typeof generatedHtmlForProduct);
+  const generated2 = compiledProductsTemplate(data.products[1]);
 
-// const generated2 = compiledProductsTemplate(data.products[1]);
+  console.log('generatedHtmlForProduct:', generated2);
 
-// console.log('generatedHtmlForProduct:', generated2);
+  const element = document.getElementById('test');
 
-// const element = document.getElementById('test');
+  console.log(element);
 
-// console.log(element);
+  function elo(htmlString) {
+    let div = document.createElement('div');
+    div.innerHTML = htmlString.trim();
+    console.log('zwroce takie cos', div.firstChild);
+    return div.firstChild;
+  }
+  element.appendChild(elo(generatedHtmlForProduct));
 
-// function elo(htmlString) {
-//   let div = document.createElement('div');
-//   div.innerHTML = htmlString.trim();
-//   console.log('zwroce takie cos', div.firstChild);
-//   return div.firstChild;
-// }
-// element.appendChild(elo(generatedHtmlForProduct));
-// });
+}
+);
+
+function connectWithDataBase() {
+
+  // let tabWithAllProducts = [];
+
+  const url = 'http://localhost:3131/products';
+
+
+
+  fetch(url)
+    .then(function (rawResponse) {
+      return rawResponse.json();
+    })
+    .then(function (parsedResponse) {
+      return parsedResponse;
+    });
+
+
+  // for (let i = 0; i <= tabWithProducts.length; i++) {
+  //   console.log(i);
+  // }
+
+}
+connectWithDataBase();
