fa87b1fa220efdf0fe6672457886097bc35d1184
Adrianna Nowak <adriannanowakk@gmail.com>
2023-05-19 17:18:11 +0200

    add 'about us' section and shadow to the images

--- src/js/script.js
+++ src/js/script.js
@@ -1,53 +1,4 @@
-// import { templates } from "./settings.js";
-// // import { select } from '../settings.js';
-
 import { settings, } from './settings.js';
-
-// const app = {
-// }
-// app.init();
-
-// import { settings } from '../settings.js';
-// const data = {
-//   'products': [
-//     {
-//       'id': 1,
-//       'description': 'Opis produktu 1',
-//       'roasting': 'Stopień palenia 1',
-//       'intensity': 'Intensywność 1',
-//       'image': 'coffee-3'
-//     },
-//     {
-//       'id': 2,
-//       'description': 'Opis produktu 2',
-//       'roasting': 'Stopień palenia 2',
-//       'intensity': 'Intensywność 2',
-//       'image': 'coffee-3'
-//     },
-//   ]
-// };
-
-// tutaj mam jeszcze pusty template
-// document.addEventListener('DOMContentLoaded', function () {
-//   // const productsTemplate = document.getElementById('products-template').innerHTML;
-//   // console.log('productsTemplate:', productsTemplate);
-
-//   // const compiledProductsTemplate = Handlebars.compile(productsTemplate);
-
-//   // const generatedHtmlForProduct = compiledProductsTemplate(parsedResponse[i]);
-
-//   // const generated2 = compiledProductsTemplate(parsedResponse[1]);
-
-//   // console.log('generated2:', generated2);
-
-//   const element = document.getElementById('test');
-
-//   console.log(element);
-
-
-//   //
-// }
-// );
 
 function elo(htmlString) {
   let div = document.createElement('div');
@@ -64,7 +15,6 @@ function connectWithDataBase() {
       return rawResponse.json();
     })
     .then(function (parsedResponse) {
-      console.log(parsedResponse);
 
       for (let i = 0; i < parsedResponse.length; i++) {
 
@@ -76,10 +26,8 @@ function connectWithDataBase() {
           const generatedHtmlForProduct = compiledProductsTemplate(parsedResponse[i]);
 
           const element = document.getElementsByClassName('container')[0];
-          console.log('elements', element);
 
           element.appendChild(elo(generatedHtmlForProduct));
-          console.log('generatedHtmlForProduct', generatedHtmlForProduct);
         }
 
         else {
@@ -91,7 +39,6 @@ function connectWithDataBase() {
           const generatedHtmlForProduct = compiledProductsTemplate(parsedResponse[i]);
 
           const element = document.getElementsByClassName('container')[0];
-          console.log('elements', element);
 
           element.appendChild(elo(generatedHtmlForProduct));
         }
